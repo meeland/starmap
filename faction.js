e280a864f14d3d07552ebf6f01473984e8b6fc67
Alexander A <47354462+meeland@users.noreply.github.com>
2026-03-12 01:58:35 +0900

    Update faction.js

--- faction.js
+++ faction.js
@@ -1,1 +1,17 @@
-
+const factionsData = {
+    "Галактическая Республика": {
+        name: "Галактическая Республика",
+        mainColor: "#91DBFF",
+        secondaryColor: "#3D4963"
+    },
+    "Конфедерация Независимых Систем": {
+        name: "Конфедерация Независимых Систем",
+        mainColor: "#FCFECD",
+        secondaryColor: "#3D375D"
+    },
+    "Нейтральные Системы": {
+        name: "Нейтральные Системы",
+        mainColor: "#a0a0a0",
+        secondaryColor: "#333333"
+    }
+};
