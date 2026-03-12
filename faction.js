ea87b19834fbb51ebbcc526ec359531cf309b378
Alexander A <47354462+meeland@users.noreply.github.com>
2026-03-12 22:12:01 +0900

    Update faction.js

--- faction.js
+++ faction.js
@@ -16,7 +16,7 @@ const factionsData = {
     },
     "Нейтральные Системы": {
         name: "Нейтральные Системы",
-        mainColor: "#a0a0a0",
-        secondaryColor: "#333333"
+        mainColor: "#00d4f4",
+        secondaryColor: "#d79800"
     }
 };
